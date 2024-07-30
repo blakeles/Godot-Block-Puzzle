64665f979d41d89323f849202b56be75f2771996
Sam <98758390+blakeles@users.noreply.github.com>
2024-07-30 23:01:13 +0100

    Update menu.gd
    Fixed crash because file isn't created

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,8 +5,11 @@ var labelText : String
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Load highscore from file and display it
-	if not FileAccess.file_exists("user://score.save"): labelText = ""
-	else: labelText = "HIGHSCORE\n" + FileAccess.open("user://score.save", FileAccess.READ).get_as_text()
+	if not FileAccess.file_exists("user://score.save"): 
+		labelText = ""
+		FileAccess.open("user://block.save", FileAccess.WRITE).store_string("0")
+	else:
+		labelText = "HIGHSCORE\n" + FileAccess.open("user://score.save", FileAccess.READ).get_as_text()
 		
 	$'CenterContainer2/Label'.text = labelText
 
